a5a93071b8daff270c2514b3158601e5d2c2a8d7
Priyanka Kammari <55157406+Priyanka2193@users.noreply.github.com>
2019-12-02 13:40:18 -0800

    input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
 
-  const [headingText,setheadingtext]= useState("Hello");
+  const [state, setstate] = useState("");
 
-  function 
+  const [headingText, setheadingtext] = useState(" ");
+
+  const [isMousedOver, setMouseOver] = useState(false);
+
+  function handleClick() {
+    //setheadingtext("Submitted");
+    setheadingtext(name);
+  }
+  function handleMouseOver() {
+    setMouseOver(true);
+  }
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  function handleChange(event) {
+    const newName = event.target.value;
+    setName(newName);
+  }
   return (
-    <div className="conatiner">
-      <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
-      <button onClick={}>Submit</button>
+    <div className="container">
+      <h1>
+        Hello <br />
+        {headingText}
+      </h1>
+      <input
+        onChange={handleChange}
+        type="text"
+        placeholder="What's your name?"
+        value={name}
+      />
+      <button
+        style={{ backgroundColor: isMousedOver ? "black" : "white" }}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        onClick={handleClick}
+      >
+        Submit
+      </button>
     </div>
   );
 }
